cfda9c324dba31ecc49a66be22be7ac9c40a71bb
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-21 18:25:38 +0100

    add models for auth

---
+++ lib/models/user_request.dart
@@ -0,0 +1,8 @@
+class RegisterRequest {
+  String email;
+  String username;
+  String password;
+
+  RegisterRequest(
+      {required this.email, required this.username, required this.password});
+}

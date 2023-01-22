95063b150defd7702f928a632fea0df59fd2e9bd
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-22 20:29:47 +0100

    refactor register page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -23,17 +23,23 @@ class _LoginScreenState extends State<LoginScreen> {
 
   _login(String email, String password) async {
     LoginRequest request = LoginRequest(email, password);
+    setState(() {
+      _loading = true;
+    });
 
     LoginResponse? res;
+    await Future.delayed(const Duration(seconds: 2));
+    setState(() {
+      _loading = false;
+    });
 
     try {
       res = await UserService.signin(request);
     } catch (e) {
-      Fluttertoast.showToast(msg: 'An error occurred!');
-    }
-
-    if (res != null) {
-      Fluttertoast.showToast(msg: 'Authenticated!');
+      Fluttertoast.showToast(
+          msg: 'An error occurred!',
+          textColor: Colors.blue,
+          backgroundColor: Colors.red);
     }
   }
 
@@ -41,107 +47,133 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xfff5f5f5),
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 100,
-                ),
-                const Center(
-                  child: Text(
-                    "Welcome back!",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 100,
                     ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 40),
-                  child: Image.asset("assets/images/login-page-image.png"),
-                ),
-                Form(
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: _emailController,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10)),
-                            ),
-                            hintStyle: TextStyle(color: Colors.black),
-                            hintText: 'Enter your email address',
-                            filled: true,
-                            fillColor: Colors.white),
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      TextFormField(
-                        controller: _passwordController,
-                        keyboardType: TextInputType.visiblePassword,
-                        obscureText: _hidePassword,
-                        decoration: InputDecoration(
-                          border: const OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                          ),
-                          hintStyle: const TextStyle(color: Colors.black),
-                          hintText: 'Enter your password',
-                          filled: true,
-                          fillColor: Colors.white,
-                          suffixIcon: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                _hidePassword = !_hidePassword;
-                              });
-                            },
-                            icon: _hidePassword
-                                ? const Icon(Icons.visibility)
-                                : const Icon(Icons.visibility_off),
-                          ),
+                    const Center(
+                      child: Text(
+                        "Welcome back!",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
                         ),
                       ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    _login(_emailController.text, _passwordController.text);
-                  },
-                  child: !_loading
-                      ? const Text("Sign in")
-                      : const SpinKitDualRing(color: Colors.white),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const RegisterScreen()));
-                  },
-                  child: const Text(
-                    "Don't have an account? Sign up",
-                    style: TextStyle(
-                      fontSize: 17,
                     ),
-                  ),
-                )
-              ],
-            ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 40),
+                      child: Image.asset("assets/images/login-page-image.png"),
+                    ),
+                    Form(
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: _emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                ),
+                                hintStyle: TextStyle(color: Colors.black),
+                                hintText: 'Enter your email address',
+                                filled: true,
+                                fillColor: Colors.white),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          TextFormField(
+                            controller: _passwordController,
+                            keyboardType: TextInputType.visiblePassword,
+                            obscureText: _hidePassword,
+                            decoration: InputDecoration(
+                              border: const OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                              ),
+                              hintStyle: const TextStyle(color: Colors.black),
+                              hintText: 'Enter your password',
+                              filled: true,
+                              fillColor: Colors.white,
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _hidePassword = !_hidePassword;
+                                  });
+                                },
+                                icon: _hidePassword
+                                    ? const Icon(Icons.visibility)
+                                    : const Icon(Icons.visibility_off),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        _login(_emailController.text, _passwordController.text);
+                      },
+                      child: const Text(
+                        "Sign in",
+                        style: TextStyle(fontSize: 19),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const RegisterScreen()));
+                      },
+                      child: const Text(
+                        "Don't have an account? Sign up",
+                        style: TextStyle(
+                          fontSize: 17,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              _loading
+                  ? Container(
+                      height: MediaQuery.of(context).size.height,
+                      color: Colors.black12,
+                      child: Center(
+                        child: Container(
+                          height: 250,
+                          width: 250,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                            color: Colors.white,
+                          ),
+                          child:
+                              const SpinKitFoldingCube(color: Colors.blueGrey),
+                        ),
+                      ),
+                    )
+                  : const SizedBox(),
+            ],
           ),
         ),
       ),
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:blog/models/user_request.dart';
+import 'package:blog/screens/login_screen.dart';
 import 'package:blog/services/user_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -16,11 +17,15 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final _emailController = TextEditingController();
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
-  bool _isLoading = false;
+  bool _loading = false;
+  bool _hidePassword = true;
 
   _register(String email, String username, String password) async {
     print('registering');
-    _isLoading = true;
+    setState(() {
+      _loading = true;
+    });
+    await Future.delayed(const Duration(seconds: 2));
     RegisterRequest payload = RegisterRequest(
       email: email,
       username: username,
@@ -35,12 +40,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
       );
     } finally {
       setState(() {
-        _isLoading = false;
+        _loading = false;
       });
-    }
-
-    if (res != null) {
-      Fluttertoast.showToast(msg: 'You have been registered');
     }
   }
 
@@ -48,94 +49,155 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xfff5f5f5),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  "Welcome back!",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                Container(
-                  margin: const EdgeInsets.only(left: 30),
-                  child: Image.asset("assets/images/register-page-image.png"),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Form(
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: _emailController,
-                        decoration: const InputDecoration(
-                            hintText: "Enter your email",
-                            fillColor: Color(0xfff5f5f5),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                            )),
-                        keyboardType: TextInputType.emailAddress,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 100,
+                    ),
+                    const Center(
+                      child: Text(
+                        "Welcome back!",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                        ),
                       ),
-                      const SizedBox(
-                        height: 20,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 40),
+                      child:
+                          Image.asset("assets/images/register-page-image.png"),
+                    ),
+                    Form(
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: _emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                ),
+                                hintStyle: TextStyle(color: Colors.black),
+                                hintText: 'Enter your email address',
+                                filled: true,
+                                fillColor: Colors.white),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          TextFormField(
+                            controller: _usernameController,
+                            keyboardType: TextInputType.text,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                ),
+                                hintStyle: TextStyle(color: Colors.black),
+                                hintText: 'Enter your username',
+                                filled: true,
+                                fillColor: Colors.white),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          TextFormField(
+                            controller: _passwordController,
+                            keyboardType: TextInputType.visiblePassword,
+                            obscureText: _hidePassword,
+                            decoration: InputDecoration(
+                              border: const OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                              ),
+                              hintStyle: const TextStyle(color: Colors.black),
+                              hintText: 'Enter your password',
+                              filled: true,
+                              fillColor: Colors.white,
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _hidePassword = !_hidePassword;
+                                  });
+                                },
+                                icon: _hidePassword
+                                    ? const Icon(Icons.visibility)
+                                    : const Icon(Icons.visibility_off),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                      TextFormField(
-                        controller: _usernameController,
-                        decoration: const InputDecoration(
-                            hintText: "Enter your username",
-                            fillColor: Color(0xfff5f5f5),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                            )),
-                        keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        _register(
+                          _emailController.text,
+                          _usernameController.text,
+                          _passwordController.text,
+                        );
+                      },
+                      child: const Text(
+                        "Sign in",
+                        style: TextStyle(fontSize: 19),
                       ),
-                      const SizedBox(
-                        height: 20,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginScreen()));
+                      },
+                      child: const Text(
+                        "Already have an account? Sign in",
+                        style: TextStyle(
+                          fontSize: 17,
+                        ),
                       ),
-                      TextFormField(
-                        controller: _passwordController,
-                        decoration: const InputDecoration(
-                            hintText: "Enter your password",
-                            fillColor: Color(0xfff5f5f5),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                            )),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                    ],
-                  ),
+                    )
+                  ],
                 ),
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 10),
-                  child: _isLoading
-                      ? const SpinKitPulse(
-                          color: Colors.red,
-                        )
-                      : null,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    _register(
-                      _emailController.text,
-                      _usernameController.text,
-                      _passwordController.text,
-                    );
-                  },
-                  child: const Text("Register"),
-                ),
-              ],
-            ),
+              ),
+              _loading
+                  ? Container(
+                      height: MediaQuery.of(context).size.height,
+                      color: Colors.black12,
+                      child: Center(
+                        child: Container(
+                          height: 250,
+                          width: 250,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                            color: Colors.white,
+                          ),
+                          child:
+                              const SpinKitFoldingCube(color: Colors.blueGrey),
+                        ),
+                      ),
+                    )
+                  : const SizedBox(),
+            ],
           ),
         ),
       ),
